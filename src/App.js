6ccceea49e050eac8f175670d8e0e1168c3833dd
yyalim <yusufyalim@gmail.com>
2021-01-09 10:54:07 +0100

    Add csv-loader to webpack

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import authors from '../data/authors.csv';
+import books from '../data/books.csv';
+import magazines from '../data/magazines.csv';
 
 export default function App() {
+  useEffect(() => {
+    console.log({ authors, books, magazines });
+  }, []);
+
   return <h1>App</h1>;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,15 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.csv$/,
+        loader: 'csv-loader',
+        options: {
+          dynamicTyping: true,
+          header: true,
+          skipEmptyLines: true
+        }
       }
     ]
   },

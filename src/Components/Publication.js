8f0a0962f739f321b1d9c81cdc397565e4c7df91
yyalim <yusufyalim@gmail.com>
2021-01-09 12:29:09 +0100

    Add authors

--- src/Components/Publication.js
+++ src/Components/Publication.js
@@ -1,12 +1,16 @@
 import React, { useContext } from 'react';
+import { AppContext } from './Store';
 
 export default function Publication(props) {
+  const [state] = useContext(AppContext)
+  const author = state.authors[props.authors]
 
   return (
     <article className="publication">
       <h2>{props.title}</h2>
       {props.description && <p>{props.description}</p>}
       {props.publishedAt && <p>Published At: {props.publishedAt}</p>}
+      {author && <p>{`${author.firstname} ${author.lastname}`}</p> }
     </article>
   )
 }
